00072678ceb85dc3cf233dda8aa0917931b64139
Jay1425 <jayraychura13@gmail.com>
2026-04-07 18:33:36 +0530

    Using the get_object_or_404 shortcut

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
@@ -10,10 +10,11 @@ def post_list(request):
     )
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
     return render(
         request,
         'blog/post/detail.html',\
