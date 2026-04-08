a77312ad4ea607ecbec581b911e93a0016bfa8de
Jay1425 <jayraychura13@gmail.com>
2026-04-08 11:14:17 +0530

    Handling pagination errors

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -7,8 +7,11 @@ def post_list(request):
 
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(
         request,
         'blog/post/list.html',
